512c72894c7ae1877c6fc09e12969616a6f41bc9
CHENG CHIEN <hcc780921@gmail.com>
2022-03-28 17:13:00 -0700

    all reffered contents structure added

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const userRoutes = require('./userRouter');
-const postRoutes = require('./postRoutes');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
-router.use('/users',userRoutes);
-router.use('/posts',postRoutes);
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
-module.exports =router;
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,15 @@
-const router = require ('express').Router();
+const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoute');
-const dashboardRoute = require('./dashboardRoutes');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/',homeRoutes);
-router.use('/api',apiRoutes);
-router.use('/',dashboardRoute);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
+
+// router.use((req, res) => {
+//   res.status(404).end();
+// });
 
 module.exports = router;
